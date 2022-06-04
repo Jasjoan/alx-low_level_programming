888d937f42fcfb9f993e909e410b3fd6a416d247
Jasjoan <judithezeugwa@gmail.com>
2022-06-04 11:57:31 -0700

    Size in C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,13 @@
 #!/bin/bash
 #include <stdio.h>
-
 /**
-
 * main - print the size of various types
-
 *
-
 * Description: prints the size of various types on the computer it is compiled
-
 * and run on
-
 *
-
 * Return: Always 0 (Success)
-
 */
-
 int main(void)
 
 {
@@ -28,9 +19,8 @@ int main(void)
     printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
     
     printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
-    
+
     printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
       
-    return (0);
-   
+    return (0);   
 }
