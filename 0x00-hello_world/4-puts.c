99bcfaeda02c507afcd000b864c782afa4e43053
Jasjoan <judithezeugwa@gmail.com>
2022-06-04 12:04:51 -0700

    Put in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,13 @@
+#!/bin/bash
 #include <stdio.h>
 
-/**
+   /**
 
- * main - Entry point
+    * main - main block
 
- *
+    * Return: 0
 
- * Description: prints "Programming is like building a multilingual puzzle"
-
- *
-
- * Return: Always 0 (Success)
-
- */
+    */
 
 int main(void)
 
