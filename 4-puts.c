5dd7e2adb471a0244f014d9076555c9d44ea4cca
Jasjoan <judithezeugwa@gmail.com>
2022-06-04 14:24:54 -0700

    Put in C

--- 4-puts.c
+++ 4-puts.c
@@ -6,15 +6,11 @@
 /**
 
  * main - Entry point
-
  *
-
  * Description: prints "Programming is like building a multilingual puzzle"
-
  *
-
  * Return: Always 0 (Success)
-
+ *
  */
 
 int main(void)
