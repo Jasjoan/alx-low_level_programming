4f7e1b1a890dbce0fe6608fa01d15a7162095bf9
Jasjoan <judithezeugwa@gmail.com>
2022-06-06 21:52:01 -0700

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,16 +3,16 @@
 /**
  * main - Prints numbers from 0 to 9.
  *
- * Return: Always 0 (Success
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int i;
- 
-        for (i = 0; i < 10; i++)
-        {
-                printf("%d", i);
-        } 
-        putchar('\n');
-       	return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
 }
